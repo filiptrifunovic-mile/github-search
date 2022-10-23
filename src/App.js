41f1ee6b08f9282da0b46a3b4f052f3586a68c2b
Filip <filip.trifunovic23@gmail.com>
2022-10-23 12:25:13 +0200

    adding pages, react-router

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Dashboard, Login, PrivateRoute, AuthWrapper, Error } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
